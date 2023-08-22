e38377e7fa7df069db2cb96220963f41a2e8a4fb
jhony2488 <jhon.araujo2488@gmail.com>
2023-08-22 13:52:52 -0300

    configurado cors da api

--- src/useApp.ts
+++ src/useApp.ts
@@ -1,5 +1,6 @@
 import express,{} from 'express';
 import swaggerUi from 'swagger-ui-express';
+import cors from 'cors';
 import routes from './routes';
 
 const swaggerFile = require('../swagger_output.json');
@@ -7,6 +8,7 @@ const swaggerFile = require('../swagger_output.json');
 export function useExpressApp(app){
     app.use(express.urlencoded({ extended: false }));
     app.use(express.json());
+    app.use(cors());
     app.use(routes);
     app.use(
       '/documentation',
